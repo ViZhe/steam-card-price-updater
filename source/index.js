3262ceb59e09aa3d5602fb12d6f2933ea30925f1
ViZhe <vizhe.cat@ya.ru>
2017-05-23 17:06:18 +0300

    back from test

--- source/index.js
+++ source/index.js
@@ -78,7 +78,7 @@ const runUpdater = setInterval(async () => {
   const result = await updateCardPrice()
   console.log(`[${result.status}] ${result.message}`)
 
-  if (result.status === 'skip' || iteration === 1) {
+  if (result.status === 'skip' || iteration === 30) {
     clearTimeout(runUpdater)
     mongoose.disconnect()
   }
